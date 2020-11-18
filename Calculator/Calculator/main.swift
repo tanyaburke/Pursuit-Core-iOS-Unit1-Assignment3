4c4b28b164d1aac7a51cf3d67321f64905598de3
tanyaburke <tanyaburke@pursuit.org>
2020-11-18 18:08:01 -0500

    Revamp/ completion of calculator app

--- Calculator/Calculator/main.swift
+++ Calculator/Calculator/main.swift
@@ -8,111 +8,183 @@
 
 import Foundation
 
+var continueCalculator = Bool()
+
 func mathStuffFactory(opString: String) -> (Double, Double) -> Double {
-  switch opString {
-  case "+":
-    return {x, y in x + y }
-  case "-":
-    return {x, y in x - y }  case "*":
-    return {x, y in x * y }
-  case "/":
-    return {x, y in x / y }
-  default:
-    return {x, y in x + y }
-  }
+    switch opString {
+    case "+":
+        return {x, y in x + y }
+    case "-":
+        return {x, y in x - y }  case "*":
+            return {x, y in x * y }
+    case "/":
+        return {x, y in x / y }
+    default:
+        return {x, y in x + y }
+    }
 }
 
 func myFilter(inputArray: [Int], by: String, filter: (Int)) -> [Int] {
-   var filterArray = [Int]()
+    var filterArray = [Int]()
     for int in inputArray {
         if by == "<" {
             if int < filter {
                 filterArray.append(int)
-               
+                
             }
         }else if by == ">" {
             if int > filter {
                 filterArray.append(int)
                 
+            }
         }
     }
-}
     return filterArray
 }
 
-//print("Please enter an expression, example: 5 + 3 ")
-//let userInput = readLine() ?? "1 + 1"
-//print()
-
-func takeInInput(input:String) -> Double {
+func myMap(inputArray: [Int], by: String, filter: (Int)) -> [Int] {
+    var mapArray = [Int]()
     
+    switch by {
+    case "*":
+        for int in inputArray {
+            mapArray.append(filter * int)
+        }
+    case "/":
+        for int in inputArray {
+            mapArray.append(int / filter)
+        }
+    default:
+        print("please input a valid operand")
+    }
+    return mapArray
+}
+func myReduce(inputArray: [Int], by: String, startAt: (Int)) -> Int {
+    var reduceResult = startAt
     
+    for int in inputArray {
+        if by == "+" {
+            reduceResult += int
+        } else if by == "*" {
+            reduceResult *= int
+        }
+    }
+    return reduceResult
+}
+func calculatorStatus() -> Bool {
+    print("Would you like to calculate again? yes or no")
+    let yesNo = readLine()?.lowercased()
+    if yesNo == "yes"{
+        return true
+    } else {
+        return false
+    }
+}
+func lowOrderMath(input:String) -> Double {
     var userInputAsArray = input.components(separatedBy: " ")
-  
+    let num1 = Double(userInputAsArray[0]) ?? 0.0
     
-      let num1 = Double(userInputAsArray[0]) ?? 0.0
-
-      let num2 = Double(userInputAsArray[2]) ?? 0.0
-      
-      let mathFunction = mathStuffFactory(opString: userInputAsArray[1])
-         let calcu = mathFunction(num1,num2)
-    let filterNumsArray = [Int]()
-   
+    let num2 = Double(userInputAsArray[2]) ?? 0.0
     
-    if userInputAsArray.contains("filter"){
-           print("Please enter the numbers you want to filter, Example: 1,5,6,7,8,9")
-        let filterNums = readLine()
-        var filterNumsArray = (filterNums?.components(separatedBy: ","),
-            print("Choose < or >")
-        let greatOrLess = readLine() ?? "<"
-        print ("Enter the number your filtering by")
-        let filterBy = (readLine() ?? "0")
-        myFilter(inputArray: (filterNumsArray.value), by:  greatOrLess, filter: filterBy.value)
-       }
+    let mathFunction = mathStuffFactory(opString: userInputAsArray[1])
+    let calcu = mathFunction(num1,num2)
     
     if userInputAsArray[1] == "?" {
         let operAndArray = ["*", "/", "+", "-"]
         userInputAsArray[1] = String(operAndArray.randomElement() ?? "*")
-       print(calcu)
+        print(calcu)
         print("Can you guess what the operator is ?")
-   let userGuess = readLine()
+        let userGuess = readLine() ?? " "
         if userGuess == userInputAsArray[1]{
             print ("That's Correct Yippeeeee")
-        }else {
-            print("Ohhh, that's wrong...")
+        } else {
+            print("Ohhh, that's wrong... the operand was \(userInputAsArray[1])")
         }
     }
- 
-//    print(userInputAsArray)
-//    print(calcu)
     return calcu
-
-    
 }
+func highOrder(userInput: String) {
+    
+    switch userInput {
+        
+    case "filter":
+        var filterNumsArray = [Int]()
+        print("Please enter the numbers you want to filter, Example: 1,5,6,7,8,9")
+        let filterNums = readLine()
+        guard let filterStringArray = (filterNums?.components(separatedBy: ",")) else { return }
+        print("Choose < or >")
+        let greatOrLess = readLine() ?? "<"
+        print ("Enter the number your filtering by")
+        let filterBy = Int(readLine() ?? "0") ?? 0
+        
+        for char in filterStringArray {
+            filterNumsArray.append(Int(char) ?? 0)
+        }
+        print("Your filtered results are \(myFilter(inputArray: (filterNumsArray), by:  greatOrLess, filter: filterBy))")
+        
+    case "map":
+        var enteredNum = [Int]()
+        print("Please enter the numbers you want to map, Example: 1,5,6,7,8,9")
+        let mapNums = readLine()
+        guard let filterStringArray = (mapNums?.components(separatedBy: ",")) else { return }
+        for char in filterStringArray {
+            enteredNum.append(Int(char) ?? 0)
+        }
+        print("Choose * or /")
+        let mapBy = readLine() ?? "*"
+        
+        print("Please enter the number you want to map by")
+        let  mapNum = Int(readLine() ?? "0") ?? 0
+        
+        print("Your map by \(mapBy) & \(mapNum) results are \(myMap(inputArray: enteredNum, by: mapBy, filter: mapNum))")
 
-
-//print(takeInInput(input: userInput))
-
-var continuePrompt = false
-
-repeat  {
- 
-   print("Please enter an expression or filter\n example: 5 + 3\n or\n filter ")
-   let userInput = readLine() ?? "1 + 1"
-  print()
-   print(takeInInput(input: userInput))
-  
-  print("Would you like to continue? yes or no")
-    let yesOrNo = readLine() ?? "no"
-    if yesOrNo.lowercased() == "yes"{
-        continuePrompt = true
-    }else{
-        continuePrompt = false
+    case "reduce":
+        var reduceNumArray = [Int]()
+        print("Please enter the numbers you want to reduce, Example: 1,5,6,7,8,9")
+        let reduceString = readLine()
+        guard let filterStringArray = (reduceString?.components(separatedBy: ",")) else { return }
+        for char in filterStringArray {
+            reduceNumArray.append(Int(char) ?? 0)
+        }
+        print("Choose * or +")
+        let reduceBy = readLine() ?? "*"
+        
+        print("Please enter the number you want to start your reduce at")
+        let  reduceNum = Int(readLine() ?? "0") ?? 0
+        
+        print("Your reduce by result is \(myReduce(inputArray: reduceNumArray, by: reduceBy, startAt: reduceNum))")
+    default:
+        print("please enter a function")
+        
     }
     
 }
-while continuePrompt == true
+print("Welcome to 🧮 Tanya's Calculator 🧮")
+print()
 
+repeat {
+    print("Choose enter 1 (Regular)  2 (High Order)")
+    let regularOrHighOrder = Int(readLine() ?? "0") ?? 0
+    
+    switch regularOrHighOrder {
+    case 1:
+        print("Enter your operation")
+        let enteredOperation = readLine() ?? "1 + 1"
+        print(lowOrderMath(input: enteredOperation))
+        continueCalculator = calculatorStatus()
+        print()
+    case 2:
+        print("Enter your operation i.e. filter, map, reduce")
+        let enteredFunction = readLine() ?? "filter"
+        highOrder(userInput: enteredFunction)
+        continueCalculator = calculatorStatus()
+        print()
+    default:
+        print("Please try again")
+        continueCalculator = true
+        print()
+    }
+}while continueCalculator
 
 
 
